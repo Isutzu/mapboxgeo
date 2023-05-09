b2bf10a3bdfb3175174510d250e63a4cfc399734
Oscar <ismael_pascual@hotmail.com>
2023-05-09 12:27:19 -0700

    remove git pages set up

--- src/Flota.js
+++ src/Flota.js
@@ -86,7 +86,7 @@ const Flota = ({ signOut }) => {
           type: "Feature",
           geometry: {
             type: "LineString",
-            coordinates: coord[0].coordinates, //
+            coordinates: coord[0].coordinates,
           },
         },
       ],
